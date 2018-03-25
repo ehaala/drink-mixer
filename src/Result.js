ff2126fee153dcb6bcf7033167abffe193755629
ehaala <ejhaala@gmail.com>
2018-03-24 20:09:24 -0700

    added background color

--- src/Result.js
+++ src/Result.js
@@ -2,9 +2,9 @@ import React, { Component } from 'react';
 import { Button, Row, Col, MenuItem } from 'react-bootstrap';
 
 const styles = {
-	background: 'lightgray',
+	background: 'black',
 	padding: '20px',
-	border: 'solid gray 1px'
+	border: 'solid white 1px',
 }
 let drinks = require('./drinks.json');
 
